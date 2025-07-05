0350b6e9cb0d87b1629014fe867e2390d83e9226
Harsh Koli <harshkoli1255@gmail.com>
2025-07-05 01:25:30 +0000

    are there any final changes you want to do?

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -1,4 +1,3 @@
-
 "use client"
 
 import * as React from "react"
@@ -81,7 +80,7 @@ const ToastClose = React.forwardRef<
     className={cn(
       "absolute right-2 top-2 rounded-md p-1 text-foreground/50 opacity-0 transition-opacity hover:text-foreground focus:opacity-100 focus:outline-none focus:ring-2 group-hover:opacity-100",
       "group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600",
-      "group-[.success]:text-primary-foreground/70 group-[.success]:hover:text-primary-foreground group-[.success]:focus:ring-primary-foreground",
+      "group-[.success]:text-success-foreground/70 group-[.success]:hover:text-success-foreground group-[.success]:focus:ring-success-foreground",
       className
     )}
     toast-close=""
